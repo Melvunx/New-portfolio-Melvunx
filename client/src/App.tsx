17a71f8a2a4b2ae186cfc85b3a2a56a4144d112c
Melvunx <augustine.melv@gmmail.com>
2024-12-06 01:44:09 +0100

    initialize project

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-ellipsis bg-red-200"></h1>;
+}
+
+export default App;
---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@compenents": path.resolve(__dirname, "./src/components"),
+      "@layout": path.resolve(__dirname, "./src/components/layout"),
+      "@pages": path.resolve(__dirname, "./src/Pages"),
+    },
+  },
+});
