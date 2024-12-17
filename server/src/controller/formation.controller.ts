6ee874dd724a35edbe7c419fe0617f409a00ec2f
Melvin <augustine.melv@gmail.com>
2024-12-17 15:30:23 +0100

    Update formation.controller.ts add formation by id

--- server/src/controller/formation.controller.ts
+++ server/src/controller/formation.controller.ts
@@ -1,15 +1,15 @@
-import { Formation } from "@/schema/aboutMe.schema";
+import { Formation } from "@schema/aboutMe.schema";
 import {
   handleSuccess,
   loggedHandleSuccess,
-} from "@/utils/handleMessageSuccess";
+} from "@utils/handleMessageSuccess";
 import pool from "@config/database";
 import { handleError } from "@utils/handleMessageError";
 import { RequestHandler } from "express";
 import { RowDataPacket } from "mysql2";
-import { loggedHandleError } from "../utils/handleMessageError";
+import { loggedHandleError } from "@utils/handleMessageError";
 
-const { GET_FORMATION } = process.env;
+const { GET_FORMATION, GET_FORMATION_ID } = process.env;
 
 export const getFormations: RequestHandler  = async (req, res) => {
   try {
@@ -21,8 +21,33 @@ export const getFormations: RequestHandler  = async (req, res) => {
   }
 
   const [formations] = await pool.query<RowDataPacket[] & Formation[]>(GET_FORMATION);
-  loggedHandleSucces("Get all formations", formations)
+  loggedHandleSucces("Get all formations", formations);
   res.status(200).json(handleSuccess("Get all formations", formations));
+  } catch(error) {
+    loggedHandleError(error);
+    res.status(500).send(handleError(error));
+    return;
+  }
+}
+
+export const getFormationId: RequestHandler = async (req, res) => {
+
+  try{
+    const { id } = req.params
+
+    if(!GET_FORMATION_ID){
+        res
+          .status(400)
+          .send(handleError(new Error("Sql request is not defined")));
+        return;
+    } else if(!id) {
+      res.status(400).send(handleError("Id required !", "Missing item !"));
+      return;
+    }
+
+  const [formation] = await pool.query<RowDataPacket[] & Formation[]>(GET_FORMATION_ID);
+  loggedHandleSucces("Get formation by id", formation);
+  res.status(200).json(handleSucces("Get formation by id", formation));
   } catch(error) {
     loggedHandleError(error);
     res.status(500).send(handleError(error));
