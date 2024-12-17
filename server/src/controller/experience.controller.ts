36e39c7fd1a81c8b735e5b7d2dc8e32310f57778
Melvunx <augustine.melv@gmmail.com>
2024-12-17 18:31:28 +0100

    fix changes

--- server/src/controller/experience.controller.ts
+++ server/src/controller/experience.controller.ts
@@ -9,48 +9,51 @@ import { RequestHandler } from "express";
 import { RowDataPacket } from "mysql2";
 import { loggedHandleError } from "../utils/handleMessageError";
 
-const { GET_EXPERIENCES } = process.env;
+const { GET_EXPERIENCES, GET_EXPERIENCE_ID } = process.env;
 
-export const getFormations: RequestHandler  = async (req, res) => {
+export const getFormations: RequestHandler = async (req, res) => {
   try {
-    if(!GET_EXPERIENCES){
-    res
+    if (!GET_EXPERIENCES) {
+      res
         .status(400)
         .send(handleError(new Error("Sql request is not defined")));
-    return;
-  }
+      return;
+    }
 
-  const [experiences] = await pool.query<RowDataPacket[] & Experience[]>(GET_EXPERIENCES);
-  loggedHandleSucces("Get all experiences", experiences)
-  res.status(200).json(handleSuccess("Get all experiences", experiences));
-  } catch(error) {
+    const [experiences] = await pool.query<RowDataPacket[] & Experience[]>(
+      GET_EXPERIENCES
+    );
+    loggedHandleSuccess("Get all experiences", experiences);
+    res.status(200).json(handleSuccess("Get all experiences", experiences));
+  } catch (error) {
     loggedHandleError(error);
     res.status(500).send(handleError(error));
     return;
   }
-}
+};
 
 export const getExperienceId: RequestHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-  try{
-    const { id } = req.params
-
-    if(!GET_EXPERIENCE_ID){
-        res
-          .status(400)
-          .send(handleError(new Error("Sql request is not defined")));
-        return;
-    } else if(!id) {
+    if (!GET_EXPERIENCE_ID) {
+      res
+        .status(400)
+        .send(handleError(new Error("Sql request is not defined")));
+      return;
+    } else if (!id) {
       res.status(400).send(handleError("Id required !", "Missing item !"));
       return;
     }
 
-  const [experience] = await pool.query<RowDataPacket[] & Experience[]>(GET_EXPERIENCE_ID);
-  loggedHandleSucces("Get experience by id", experience);
-  res.status(200).json(handleSucces("Get experience by id", experience));
-  } catch(error) {
+    const [experience] = await pool.query<RowDataPacket[] & Experience[]>(
+      GET_EXPERIENCE_ID
+    );
+    loggedHandleSuccess("Get experience by id", experience);
+    res.status(200).json(handleSuccess("Get experience by id", experience));
+  } catch (error) {
     loggedHandleError(error);
     res.status(500).send(handleError(error));
     return;
   }
-}
+};
--- server/src/controller/formation.controller.ts
+++ server/src/controller/formation.controller.ts
@@ -1,56 +1,58 @@
+import pool from "@config/database";
 import { Formation } from "@schema/aboutMe.schema";
+import { handleError, loggedHandleError } from "@utils/handleMessageError";
 import {
   handleSuccess,
   loggedHandleSuccess,
 } from "@utils/handleMessageSuccess";
-import pool from "@config/database";
-import { handleError } from "@utils/handleMessageError";
 import { RequestHandler } from "express";
 import { RowDataPacket } from "mysql2";
-import { loggedHandleError } from "@utils/handleMessageError";
 
 const { GET_FORMATION, GET_FORMATION_ID } = process.env;
 
-export const getFormations: RequestHandler  = async (req, res) => {
+export const getFormations: RequestHandler = async (req, res) => {
   try {
-    if(!GET_FORMATION){
-    res
+    if (!GET_FORMATION) {
+      res
         .status(400)
         .send(handleError(new Error("Sql request is not defined")));
-    return;
-  }
+      return;
+    }
 
-  const [formations] = await pool.query<RowDataPacket[] & Formation[]>(GET_FORMATION);
-  loggedHandleSucces("Get all formations", formations);
-  res.status(200).json(handleSuccess("Get all formations", formations));
-  } catch(error) {
+    const [formations] = await pool.query<RowDataPacket[] & Formation[]>(
+      GET_FORMATION
+    );
+    loggedHandleSuccess("Get all formations", formations);
+    res.status(200).json(handleSuccess("Get all formations", formations));
+  } catch (error) {
     loggedHandleError(error);
     res.status(500).send(handleError(error));
     return;
   }
-}
+};
 
 export const getFormationId: RequestHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-  try{
-    const { id } = req.params
-
-    if(!GET_FORMATION_ID){
-        res
-          .status(400)
-          .send(handleError(new Error("Sql request is not defined")));
-        return;
-    } else if(!id) {
+    if (!GET_FORMATION_ID) {
+      res
+        .status(400)
+        .send(handleError(new Error("Sql request is not defined")));
+      return;
+    } else if (!id) {
       res.status(400).send(handleError("Id required !", "Missing item !"));
       return;
     }
 
-  const [formation] = await pool.query<RowDataPacket[] & Formation[]>(GET_FORMATION_ID);
-  loggedHandleSucces("Get formation by id", formation);
-  res.status(200).json(handleSucces("Get formation by id", formation));
-  } catch(error) {
+    const [formation] = await pool.query<RowDataPacket[] & Formation[]>(
+      GET_FORMATION_ID
+    );
+    loggedHandleSuccess("Get formation by id", formation);
+    res.status(200).json(handleSuccess("Get formation by id", formation));
+  } catch (error) {
     loggedHandleError(error);
     res.status(500).send(handleError(error));
     return;
   }
-}
+};
